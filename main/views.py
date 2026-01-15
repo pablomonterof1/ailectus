781f55cab24c80fa0961a6dd73d1c91b68582861
pablomonterof1 <pablo.montero.farias@outlook.com>
2026-01-15 10:57:44 -0500

    15012026PM

--- main/views.py
+++ main/views.py
@@ -18,13 +18,13 @@ def contacto_view(request):
             nombre = form.cleaned_data["nombre"]
             email = form.cleaned_data["email"]
             mensaje = form.cleaned_data["mensaje"]
-
+            mensaje_html = mensaje.replace("\n", "<br>")
             asunto = f"Nuevo contacto desde Ailectus – {nombre}"
             cuerpo_html = f"""
                 <h3>Nuevo mensaje de contacto</h3>
                 <p><b>Nombre:</b> {nombre}</p>
                 <p><b>Email:</b> {email}</p>
-                <p><b>Mensaje:</b><br>{mensaje.replace('\n', '<br>')}</p>
+                <p><b>Mensaje:</b><br>{mensaje_html}</p>
             """
             email_msg = EmailMessage(
                 subject=asunto,
